998c4f9391e683018f4a67abfd5c5fe665f647bc
Ivan <carealwhay@gmail.com>
2021-03-09 18:51:13 +1000

    fields update

--- weather/models.py
+++ weather/models.py
@@ -12,6 +12,10 @@ class Description(models.Model):
     weather_description = models.IntegerField(choices=DESCRIPTIONS, default=0)
     temperature = models.FloatField()
     created_on = models.DateTimeField(auto_now_add=True)
+    probOfPrecip = models.IntegerField(default=0)
+    wind = models.IntegerField(default=0)
+    wet = models.IntegerField(default=0)
+    time = models.TextField(default=0)
 
     class Meta:
         ordering = ['-created_on']
